8ca5f6acc16de9f555a2dc496a0d2a1ad6cd9645
lmartinezh <linloremartiz@gmail.com>
2021-02-09 18:39:47 -0500

    create files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* print_alphabet - print the alphabet on screen
+*
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+		_putchar(character);
+	_putchar('\n');
+
+}
