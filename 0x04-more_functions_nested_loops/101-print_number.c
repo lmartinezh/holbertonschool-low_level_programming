019c187cb50c5ffa2cbc6f8545d2fc71d9c9ca7b
lmartinezh <linloremartiz@gmail.com>
2021-02-11 21:39:22 -0500

    update file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,6 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
+#include <math.h>
 /**
 * main - Entry point
 *
@@ -7,6 +8,11 @@
 */
 int main(void)
 {
-	
+	long i, numbre = 612852475143;
+
+	for (i = 2; i < sqrt(numbre); i++)
+		while ((numbre % i) == 0)
+			numbre /= i;
+	printf("%ld\n", numbre);
 	return (0);
 }
