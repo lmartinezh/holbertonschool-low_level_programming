2252fc2c63b451e685a626a4b5adfde0d82f7500
lmartinezh <linloremartiz@gmail.com>
2021-02-11 20:33:23 -0500

    create file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* print_numbers - function that prints the numbers, from 0 to 9,
+*                 followed by a new line.
+* Return: 1 (is uppercase) else 1
+*/
+void print_numbers(void)
+{
+	char number = '/';
+
+	while ((number++) < '9')
+		_putchar(number);
+	_putchar('\n');
+
+}
