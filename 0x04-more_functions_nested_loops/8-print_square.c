c1bf7d4e841b66373e9fba428770d2183b71e9c1
lmartinezh <linloremartiz@gmail.com>
2021-02-11 21:23:06 -0500

    create file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* add_space - print spaces
+* @space: int lines
+*/
+void add_space(int space)
+{
+	if (space > 1)
+	{
+		_putchar('#');
+		add_space(--space);
+	}
+}
+/**
+* print_square - Entry point
+* @n: int
+* Return: Always 0 (Success)
+*/
+void print_square(int n)
+{
+	const int TOP = n;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (n = 0; n < TOP; n++)
+		{
+			add_space(TOP);
+			_putchar('#');
+			_putchar('\n');
+		}
+}
