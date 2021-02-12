0402eff7ebce1c1448d00dc107b2dc42d27c3e3e
lmartinezh <linloremartiz@gmail.com>
2021-02-11 21:02:09 -0500

    create file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_line - Entry point
+*
+* @n: int
+*
+* Return: Always 0 (Success)
+*/
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		_putchar('_');
+		print_line(n - 1);
+	}
+}
